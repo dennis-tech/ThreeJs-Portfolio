5061217f94c1cde4ba1db89d9f1d9896e24c8c46
unknown <hedenniskibe@gmail.com>
2025-01-03 04:29:16 -0500

    added vercel analytics

--- src/App.jsx
+++ src/App.jsx
@@ -6,6 +6,7 @@ import Clients from "./sections/Clients.jsx";
 import Contact from "./sections/Contact.jsx";
 import Footer from "./sections/Footer.jsx";
 import Experience from "./sections/Experience.jsx";
+import { Analytics } from "@vercel/analytics/react"
 
 const App = () => {
     return (
